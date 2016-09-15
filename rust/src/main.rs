a7388f38ff7ce07250712656a2ed4f39d488432e
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-09-16 08:15:21 +0900

    Ready to save msgpack

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,15 @@
+
+extern crate rmp_serialize;
+extern crate rustc_serialize;
+
+use rustc_serialize::Encodable;
+use rmp_serialize::Encoder;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let val = (42u8, "the Answer");
+    let mut buf = File::create("foo.msg").ok().unwrap();
+    let mut enc = Encoder::new(&mut buf);
+    let res = val.encode(&mut enc);
+    println!("{:?}", res);
 }
