ea3b45b5a87c7d8c062aa7cba6b8d879fbd6d537
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-09-16 12:32:03 +0900

    Import sample of ndarray-odeint

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,10 @@
 
+extern crate ndarray;
+extern crate ndarray_odeint;
 extern crate rmp_serialize;
 extern crate rustc_serialize;
 
+use ndarray::prelude::*;
 use rustc_serialize::Encodable;
 use rmp_serialize::Encoder;
 use std::fs::File;
@@ -12,4 +15,12 @@ fn main() {
     let mut enc = Encoder::new(&mut buf);
     let res = val.encode(&mut enc);
     println!("{:?}", res);
+
+    let mut x = arr1(&[1.0, 0.0, 0.0]);
+    let l = |y| ndarray_odeint::lorenz63(10., 28., 8.0 / 3.0, y);
+    let teo = |y| ndarray_odeint::rk4(&l, 0.01, y);
+    for _ in 0..1000000 {
+        x = teo(x);
+    }
+    println!("{:?}", x);
 }
