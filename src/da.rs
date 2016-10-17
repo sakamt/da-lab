d8993290c101802dd24cf82713df12755c00fe96
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-10-18 03:06:01 +0900

    Generate init

--- src/da.rs
+++ src/da.rs
@@ -12,7 +12,7 @@ pub type V = Array<f64, Ix>;
 pub type M = Array<f64, (Ix, Ix)>;
 pub type Ensemble = Vec<V>;
 
-fn teo(dt: f64, step: usize, mut x: V) -> V {
+pub fn teo(dt: f64, step: usize, mut x: V) -> V {
     let l = |y| lorenz63(10., 28., 8.0 / 3.0, y);
     let u = |y| rk4(&l, dt, y);
     for _ in 0..step {
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 
+extern crate rand;
 extern crate ndarray;
+extern crate ndarray_rand;
 extern crate rustc_serialize;
 extern crate aics_da;
 
 use std::fs;
+use rand::distributions::*;
 use ndarray::prelude::*;
+use ndarray_rand::RandomExt;
 use aics_da::*;
 
 #[derive(RustcDecodable)]
@@ -12,18 +16,26 @@ struct Setting {
     count: usize,
     k: usize,
     tau: usize,
+    save_count: usize,
     dt: f64,
 }
 
 fn main() {
     let setting: Setting = io::read_json("setting.json");
-    let mut xs = vec![Array::range(1., 4., 1.); setting.k ];
     fs::create_dir_all("data").unwrap();
+
+    let mut x = arr1(&[1., 0., 0.]);
+    let mut xs = Vec::new();
+    let dist = Normal::new(0.0, 1.0);
+    for _ in 0..setting.k {
+        xs.push(Array::random(3, dist));
+    }
     for t in 0..setting.count {
-        xs = da::forcast(xs, setting.dt, 1);
-        let fname = format!("data/{:05}.msg", t);
-        if t % setting.tau == 0 {
-            // TODO analysis
+        x = da::teo(setting.dt, setting.tau, x);
+        xs = da::forcast(xs, setting.dt, setting.tau);
+        // TODO analysis
+        if t % setting.save_count == 0 {
+            let fname = format!("data/{:05}.msg", t);
             io::save_as_msg(&xs, fname);
         }
     }
