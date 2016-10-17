dd70b4d16915fa74a3b700c363be0c45376f7e79
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-10-18 03:14:51 +0900

    Minor rev

--- src/main.rs
+++ src/main.rs
@@ -25,18 +25,21 @@ fn main() {
     fs::create_dir_all("data").unwrap();
 
     let mut x = arr1(&[1., 0., 0.]);
-    let mut xs = Vec::new();
+    let mut xs: Vec<da::V> = Vec::new();
     let dist = Normal::new(0.0, 1.0);
     for _ in 0..setting.k {
-        xs.push(Array::random(3, dist));
+        xs.push(0.01 * Array::random(3, dist) + &x);
     }
     for t in 0..setting.count {
         x = da::teo(setting.dt, setting.tau, x);
         xs = da::forcast(xs, setting.dt, setting.tau);
         // TODO analysis
         if t % setting.save_count == 0 {
-            let fname = format!("data/{:05}.msg", t);
-            io::save_as_msg(&xs, fname);
+            let tt = t / setting.save_count;
+            let xs_fname = format!("data/xs{:05}.msg", tt);
+            io::save_as_msg(&xs, xs_fname);
+            let x_fname = format!("data/x{:05}.msg", tt);
+            io::save_as_msg(&x, x_fname);
         }
     }
 }
