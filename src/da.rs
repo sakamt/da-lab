bc1c906ced42cecd94a19c85e392f0ff02b2b441
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-10-25 14:51:59 +0900

    Introduce da::random

--- src/da.rs
+++ src/da.rs
@@ -14,17 +14,21 @@ pub fn forcast(teo: &Fn(V) -> V, xs: Ensemble) -> Ensemble {
     xs.into_iter().map(teo).collect()
 }
 
+pub fn random(n: usize) -> V {
+    let dist = Normal::new(0., 1.0);
+    Array::random(n, dist)
+}
+
 pub fn enkf(xs: Ensemble, y: &V, h: &M, r: &M) -> Ensemble {
     let (_, p) = stat2(&xs);
     let v = h.dot(&p).dot(&h.t()) + r;
     let vinv = v.inv().unwrap();
     let k = p.dot(&h.t()).dot(&vinv);
     let rs = r.clone().ssqrt().unwrap();
-    let dist = Normal::new(0., 1.0);
     let n = y.len();
     xs.into_iter()
         .map(|x| {
-            let err = y - &h.dot(&x) + rs.dot(&Array::random(n, dist));
+            let err = y - &h.dot(&x) + rs.dot(&random(n));
             x + k.dot(&err)
         })
         .collect()
