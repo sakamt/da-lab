d7cdeb09da72c10ddda7e2dc242a5fd38e9bd0b0
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-11-24 13:45:16 +0900

    Upgrade to ndarray v0.7

--- src/einsum.rs
+++ src/einsum.rs
@@ -2,7 +2,7 @@
 
 use ndarray::prelude::*;
 
-pub fn a_b__ab(va: &Array<f64, Ix>, vb: &Array<f64, Ix>) -> Array<f64, (Ix, Ix)> {
+pub fn a_b__ab(va: &Array<f64, Ix1>, vb: &Array<f64, Ix1>) -> Array<f64, Ix2> {
     let na = va.len();
     let nb = vb.len();
     let mut res = Array::zeros((na, nb));
