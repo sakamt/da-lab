6c6f1cb1f17707de7494d4b8d4a1df0f66ef47db
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-12-14 18:17:20 +0900

    Add load_msg

--- rust/src/io.rs
+++ rust/src/io.rs
@@ -1,14 +1,20 @@
 
 use rustc_serialize::{Encodable, Decodable, json};
-use rmp_serialize::Encoder;
+use rmp_serialize::{Encoder, Decoder};
 use std::fs::File;
 use std::io::Read;
 use std::string::String;
 
-pub fn save_as_msg<T: Encodable>(val: &T, filename: String) {
+pub fn save_msg<T: Encodable>(val: &T, filename: &str) {
     let mut buf = File::create(filename).ok().unwrap();
     let mut enc = Encoder::new(&mut buf);
     val.encode(&mut enc).unwrap();
+}
+
+pub fn load_msg<T: Decodable>(filename: &str) -> T {
+    let mut buf = File::open(filename).unwrap();
+    let mut dec = Decoder::new(&mut buf);
+    Decodable::decode(&mut dec).unwrap()
 }
 
 pub fn read_json<Contents: Decodable>(filename: &str) -> Contents {
