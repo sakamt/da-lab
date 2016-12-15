78faeeae2e96c173e5029ebfac6c5b397ed53735
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-12-15 18:37:48 +0900

    Add non_gaussianity

--- python/aics_da/__init__.py
+++ python/aics_da/__init__.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import scipy
 import msgpack
 
 
@@ -13,3 +14,14 @@ def save_msg(a, filename):
     data = (1, a.shape, a.flatten().tolist())
     with open(filename, "wb") as f:
         msgpack.dump(data, f)
+
+
+def non_gaussianity(xs, l, plot=False):
+    xm = np.average(xs, axis=1)
+    P = np.cov(xs)
+    kernel = scipy.stats.gaussian_kde(xs)
+    gaussian = scipy.stats.multivariate_normal(xm, P)
+    ys = np.array([np.random.multivariate_normal(xm, P) for _ in range(l)]).T
+    k = kernel.logpdf(ys)
+    g = gaussian.logpdf(ys.T)
+    return np.average(g - k)
