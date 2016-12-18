32c7ed2cc6752217671790ebb2b3e3e84435b970
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-12-18 15:05:23 +0900

    Save obs and init

--- rust/run.sh
+++ rust/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
-set -x
+# TODO check args
 time cargo run --release --bin=l63_init $1
 time cargo run --release --bin=l63_genobs $1 init.msg
-time cargo run --release --bin=l63_enkf $1 obs.msg init.msg $(date +%Y%m%d-%H%M%S)
+output=$(date +%Y%m%d-%H%M%S)
+time cargo run --release --bin=l63_enkf $1 obs.msg init.msg $output
+cp $1 init.msg obs.msg $output
