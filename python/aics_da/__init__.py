dc2d8d6804d99b890fff137027511208e8b4762b
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-12-19 17:23:58 +0900

    Merge DA -> aics_da

--- python/aics_da/__init__.py
+++ python/aics_da/__init__.py
@@ -1,2 +1,13 @@
 # -*- coding: utf-8 -*-
-__all__ = ["io", "ensemble"]
+
+import os.path as op
+from glob import glob
+from importlib import import_module
+
+
+__all__ = [op.basename(f)[:-3]
+           for f in glob(op.join(op.dirname(__file__), "*.py"))
+           if op.basename(f) != "__init__.py"
+           if not op.basename(f).startswith("test_")]
+
+_mods = [import_module("aics_da." + m) for m in __all__]
