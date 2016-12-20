69b224c90c2ea68dcfe297135f3dc4225456fbe4
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-12-20 19:39:37 +0900

    Add load_ensemble, load_msg

--- python/aics_da/io.py
+++ python/aics_da/io.py
@@ -2,6 +2,12 @@
 
 import numpy as np
 import msgpack
+from glob import glob
+
+
+def load_msg(filename):
+    with open(filename, "rb") as f:
+        return msgpack.load(f)
 
 
 def load_array(filename):
@@ -26,3 +32,17 @@ def save_arrays(as_, filename):
     data = [(1, a.shape, a.flatten().tolist()) for a in as_]
     with open(filename, "wb") as f:
         msgpack.dump(data, f)
+
+
+def load_ensembles(data_dir, return_length=False):
+    T = len(glob(data_dir + "a*.msg"))
+
+    def gen():
+        for t in range(T):
+            xs_b = load_arrays(data_dir + "b{:05d}.msg".format(t)).T
+            xs_a = load_arrays(data_dir + "a{:05d}.msg".format(t)).T
+            yield (xs_b, xs_a)
+    if return_length:
+        return T, gen()
+    else:
+        return gen()
