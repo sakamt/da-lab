c57a7658427f9377522d9741c1abcebe04a2b4e5
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-12-25 11:45:49 +0900

    Implement MergeResampler (not tested)

--- rust/src/mpf.rs
+++ rust/src/mpf.rs
@@ -1,22 +1,40 @@
 
+use rand;
+use rand::distributions::IndependentSample;
+use num_traits::float::Float;
+use ndarray::prelude::*;
+
 use ensemble::*;
 use weight::*;
-use num_traits::float::Float;
 
 /// Coefficient for merge resampling
 #[derive(Clone, Debug)]
-pub struct MergeCoef(Vec<f64>);
-
-impl Default for MergeCoef {
-    fn default() -> Self {
-        MergeCoef(vec![3.0 / 4.0, (13.0.sqrt() + 1.0) / 8.0, -(13.0.sqrt() - 1.0) / 8.0])
-    }
-    // fn random(n: usize) -> Self {}
+pub struct MergeResampler {
+    coef: Vec<f64>,
 }
 
-impl MergeCoef {
-    pub fn resampling(_: &Weight, xs: Ensemble) -> Ensemble {
-        // TODO
-        xs
+impl Default for MergeResampler {
+    fn default() -> Self {
+        MergeResampler { coef: vec![3.0 / 4.0, (13.0.sqrt() + 1.0) / 8.0, -(13.0.sqrt() - 1.0) / 8.0] }
+    }
+}
+
+impl MergeResampler {
+    pub fn resampling(&self, w: &Weight, xs: &Ensemble) -> Ensemble {
+        let n = xs[0].len();
+        let k = xs.len();
+        let dist = w.to_dist();
+        let mut rng = rand::thread_rng();
+        (0..k)
+            .map(|_| {
+                self.coef
+                    .iter()
+                    .map(|&coef| {
+                        let idx = dist.ind_sample(&mut rng);
+                        coef * &xs[idx]
+                    })
+                    .fold(Array::zeros(n), |sum, x| sum + x)
+            })
+            .collect()
     }
 }
