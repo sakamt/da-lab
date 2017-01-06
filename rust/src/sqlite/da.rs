59eaf88d2f57929270ed232f897da59587d0a375
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-06 15:32:49 +0900

    Save meta info

--- rust/src/sqlite/da.rs
+++ rust/src/sqlite/da.rs
@@ -2,16 +2,16 @@
 use rusqlite::Connection;
 use super::super::da::Setting;
 
-pub fn insert_enkf(setting: &Setting, ensemble: &str, truth: &str, observable: &str, conn: &Connection) -> i32 {
-    conn.execute(&"INSERT INTO enkf(K, tau, count, r, dt, ensemble, truth, observable) values (?1, ?2, ?3, ?4, ?5, \
-                   ?6, ?7, ?8);",
+pub fn insert_enkf(setting: &Setting, truth_id: i64, observable_id: i64, ensemble_id: i64, conn: &Connection) -> i32 {
+    conn.execute(&"INSERT INTO enkf(K, tau, count, r, dt, truth_id, observation_id, ensemble_id) values (?1, ?2, ?3, \
+                   ?4, ?5, ?6, ?7, ?8);",
                  &[&(setting.k as i64),
                    &(setting.tau as i64),
                    &(setting.count as i64),
                    &setting.r,
                    &setting.dt,
-                   &ensemble,
-                   &truth,
-                   &observable])
+                   &truth_id,
+                   &observable_id,
+                   &ensemble_id])
         .expect("Missing to insert EnKF result")
 }
--- rust/src/sqlite/ensemble_ts.rs
+++ rust/src/sqlite/ensemble_ts.rs
@@ -1,11 +1,35 @@
 
 use rusqlite::Connection;
 
-pub fn generate_table_name(postfix: &str) -> String {
+pub struct EnsembleTS<'a> {
+    table_name: String,
+    conn: &'a Connection,
+}
+
+impl<'a> EnsembleTS<'a> {
+    pub fn new(conn: &'a Connection, postfix: &str) -> Self {
+        let tb_name = generate_table_name(postfix);
+        create_table(conn, &tb_name);
+        EnsembleTS {
+            table_name: tb_name,
+            conn: conn,
+        }
+    }
+
+    pub fn insert(&self, time: f64, forecasted: &str, analysized: &str) {
+        insert(time, forecasted, analysized, self.conn, &self.table_name);
+    }
+
+    pub fn register(&self, dt: f64, k: usize, truth_id: i64, observation_id: i64) -> i64 {
+        register(dt, k, truth_id, observation_id, &self.table_name, self.conn)
+    }
+}
+
+fn generate_table_name(postfix: &str) -> String {
     format!("_ensemble_ts_{}", postfix)
 }
 
-pub fn create_table(conn: &Connection, table_name: &str) {
+fn create_table(conn: &Connection, table_name: &str) {
     let sql = format!(r#"CREATE TABLE {} (
                            time REAL NOT NULL,
                            forecasted TEXT NOT NULL,
@@ -15,7 +39,15 @@ pub fn create_table(conn: &Connection, table_name: &str) {
     conn.execute(&sql, &[]).expect("Fail to create ensemble timeserise table");
 }
 
-pub fn insert(time: f64, forecasted: &str, analysized: &str, conn: &Connection, table_name: &str) {
+fn register(dt: f64, k: usize, truth_id: i64, observation_id: i64, table_name: &str, conn: &Connection) -> i64 {
+    let sql = "INSERT INTO ensemble (table_name, dt, K, truth_id, observation_id) VALUES (?1, ?2, ?3, ?4, ?5)";
+    conn.execute(&sql,
+                 &[&table_name, &dt, &(k as i64), &truth_id, &observation_id])
+        .expect("Failed to register ensemble timeserise");
+    conn.last_insert_rowid()
+}
+
+fn insert(time: f64, forecasted: &str, analysized: &str, conn: &Connection, table_name: &str) {
     let sql = format!("INSERT INTO {} values (?1, ?2, ?3);", &table_name);
     conn.execute(&sql, &[&time, &forecasted, &analysized]).expect("miss to insert ensemble_ts");
 }
