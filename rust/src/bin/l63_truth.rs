b2cbd56baed39ba7c98a62961958998978581a15
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-17 15:45:38 +0900

    Move generate_truth into l63::

--- rust/src/bin/l63_truth.rs
+++ rust/src/bin/l63_truth.rs
@@ -1,18 +1,13 @@
-#![allow(non_snake_case)]
 
 extern crate ndarray;
 extern crate rustc_serialize;
 extern crate docopt;
 extern crate aics_da;
-extern crate itertools;
 extern crate rusqlite;
 
 use docopt::Docopt;
-use ndarray::prelude::*;
 use aics_da::*;
-use aics_da::types::V;
 use aics_da::sqlite as sql;
-use itertools::iterate;
 
 const USAGE: &'static str = "
 Generate truth of Lorenz63 model
@@ -32,21 +27,12 @@ fn main() {
     let setting: da::Setting = io::read_json(&args.arg_setting);
     let mut conn = rusqlite::Connection::open(args.arg_db).unwrap();
     let tx = conn.transaction().unwrap();
-    gen_truth(setting, &tx);
-    tx.commit().unwrap();
-}
-
-fn gen_truth(setting: da::Setting, conn: &rusqlite::Connection) {
-    let T = setting.tau * setting.count;
-    let x0: V = arr1(&[1.0, 0.0, 0.0]);
-    let truth: Vec<V> = iterate(x0, |x| l63::teo(setting.dt, 1, x.clone()))
-        .skip(T / 10)
-        .take(T)
-        .collect();
+    let truth = l63::generate_truth(&setting);
     let tid = sql::save_truth(setting.dt,
-                              T as f64 * setting.dt,
+                              (setting.tau * setting.count) as f64 * setting.dt,
                               &truth,
-                              &conn,
+                              &tx,
                               &sql::util::now_str());
+    tx.commit().unwrap();
     println!("{}", tid);
 }
