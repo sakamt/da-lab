b4fea419b0e7f2d40deb19b129130a5a21e72919
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-17 12:34:52 +0900

    Remove global ensemble table

--- rust/src/sqlite/da.rs
+++ rust/src/sqlite/da.rs
@@ -2,9 +2,15 @@
 use rusqlite::Connection;
 use super::super::da::Setting;
 
-pub fn insert_enkf(setting: &Setting, truth_id: i64, observable_id: i64, ensemble_id: i64, conn: &Connection) -> i32 {
-    conn.execute(&"INSERT INTO enkf(K, tau, count, r, dt, truth_id, observation_id, ensemble_id) values (?1, ?2, ?3, \
-                   ?4, ?5, ?6, ?7, ?8);",
+pub fn insert_enkf(setting: &Setting,
+                   truth_id: i64,
+                   observable_id: i64,
+                   ensemble_table: &str,
+                   conn: &Connection)
+                   -> i32 {
+    conn.execute(&r#"INSERT INTO enkf(K, tau, count, r, dt, truth_id,
+                                      observation_id, ensemble_table)
+                     VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8);"#,
                  &[&(setting.k as i64),
                    &(setting.tau as i64),
                    &(setting.count as i64),
@@ -12,6 +18,6 @@ pub fn insert_enkf(setting: &Setting, truth_id: i64, observable_id: i64, ensembl
                    &setting.dt,
                    &truth_id,
                    &observable_id,
-                   &ensemble_id])
+                   &ensemble_table])
         .expect("Missing to insert EnKF result")
 }
