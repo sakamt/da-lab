bc51c488b5a103bc50ac197f8a12b7642ef7e14c
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-20 14:08:29 +0900

    Refactoring timeseries adopting to io::SeriesStorage

--- rust/src/sqlite/mod.rs
+++ rust/src/sqlite/mod.rs
@@ -1,12 +1,13 @@
 
-pub mod util;
+pub mod storage;
 pub mod timeseries;
 pub mod ensemble;
 pub mod ensemble_ts;
 pub mod da;
 pub mod stat;
+pub mod util;
 
+pub use self::storage::SqliteStorage;
 pub use self::ensemble::save_ensemble;
 pub use self::ensemble_ts::EnsembleTS;
-pub use self::timeseries::{save_truth, save_observation, get_truth, get_observation};
 pub use self::stat::StatTS;
