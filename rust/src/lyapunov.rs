f4b4075dd72b321e697c24d51e01f00c7b2a5e32
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-01-30 19:52:22 +0900

    Rewrite jacobi_cached using Dot<M>

--- rust/src/lyapunov.rs
+++ rust/src/lyapunov.rs
@@ -19,16 +19,7 @@ pub fn jacobi_cached<F>(f: &F, x0: &V, alpha: f64) -> M
     where F: Fn(V) -> V
 {
     let n = x0.len();
-    let nx0 = x0.norm();
-    let fx0 = f(x0.clone());
-    hstack(&(0..n)
-            .map(|i| {
-                let mut ei = Array::zeros(n);
-                ei[i] = nx0 * alpha;
-                f(ei + x0) - &fx0
-            })
-            .collect())
-        .unwrap() / (nx0 * alpha)
+    f.jacobian(x0, alpha).dot(&Array::eye(n))
 }
 
 pub struct Jacobian<'a, 'b, TEO>
