8f145caa6527db61e292c0dea53314440c5ae59c
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-02-01 21:08:12 +0900

    Add Induce trait for settings

--- rust/src/settings.rs
+++ rust/src/settings.rs
@@ -1,12 +1,40 @@
 
+use super::da;
+
+pub trait Induce<Dest> {
+    fn induce(&self) -> Dest;
+}
+
+#[derive(RustcDecodable, Debug, Clone)]
 pub struct Truth {
     pub dt: f64,
     pub length: usize,
 }
 
+#[derive(RustcDecodable, Debug, Clone)]
 pub struct Observation {
     pub dt: f64,
     pub tau: usize,
     pub count: usize,
     pub r: f64,
+}
+
+impl Induce<Truth> for da::Setting {
+    fn induce(&self) -> Truth {
+        Truth {
+            dt: self.dt,
+            length: self.tau * self.count,
+        }
+    }
+}
+
+impl Induce<Observation> for da::Setting {
+    fn induce(&self) -> Observation {
+        Observation {
+            dt: self.dt,
+            tau: self.tau,
+            count: self.count,
+            r: self.r,
+        }
+    }
 }
