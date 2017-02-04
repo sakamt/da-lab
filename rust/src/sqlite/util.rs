a0c54e8eae5ef2035ac3876ee34a67e7cce3915c
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-02-04 18:40:44 +0900

    Revise initialization

--- rust/src/sqlite/util.rs
+++ rust/src/sqlite/util.rs
@@ -1,10 +1,10 @@
 
-use std::path::Path;
 use time;
 use uuid::Uuid;
 use rusqlite::Connection;
 
 const INIT_SQL: &'static str = r#"
+BEGIN;
 CREATE TABLE IF NOT EXISTS enkf (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   K INTEGER NOT NULL,
@@ -32,6 +32,7 @@ CREATE TABLE IF NOT EXISTS observation (
   r REAL NOT NULL,
   truth_id INTEGER NOT NULL
 );
+COMMIT;
 "#;
 
 pub fn now_str() -> String {
@@ -39,17 +40,19 @@ pub fn now_str() -> String {
     time::strftime("%Y%m%d_%H%M%S", &tm).unwrap()
 }
 
+/// generate table name using UUID v4 (rnadom UUID)
 pub fn generate_table_name(prefix: &str) -> String {
     format!("{}_{}", prefix, Uuid::new_v4())
 }
 
+/// Create pre-defined Tables
+pub fn init_db(conn: &Connection) {
+    conn.execute_batch(&INIT_SQL).expect("Failed to init");
+}
+
+/// Open SQLite DB with initialization
 pub fn open_with_init(dbname: &str) -> Connection {
-    let dbpath = Path::new(dbname);
-    if dbpath.exists() {
-        Connection::open(dbpath).expect("Failed to open DB")
-    } else {
-        let db = Connection::open(dbpath).expect("Failed to open DB");
-        db.execute(&INIT_SQL, &[]).expect("Failed to init");
-        db
-    }
+    let db = Connection::open(dbname).expect("Failed to open DB");
+    init_db(&db);
+    db
 }
--- rust/tests/sqlite.rs
+++ rust/tests/sqlite.rs
@@ -9,21 +9,19 @@ use aics_da::io::*;
 
 #[test]
 fn io_truth() {
-    let conn = rusqlite::Connection::open("test.db").unwrap();
-    let storage = sqlite::SqliteStorage::new(&conn);
+    let conn = sqlite::open_with_init("test_io_truth.db");
     let truth = vec![arr1(&[1.0, 0.0, 0.0]), arr1(&[0.0, 1.0, 0.0])];
     let setting = settings::Truth {
         dt: 0.01,
         length: 100,
     };
-    let tid = storage.save_truth(&setting, &truth);
-    let _ = storage.load_truth(tid);
+    let tid = conn.save_truth(&setting, &truth);
+    let _ = conn.load_truth(tid);
 }
 
 #[test]
 fn io_observation() {
-    let conn = rusqlite::Connection::open("test.db").unwrap();
-    let storage = sqlite::SqliteStorage::new(&conn);
+    let conn = sqlite::open_with_init("test_io_observation.db");
     let obs = vec![arr1(&[1.0, 0.0, 0.0]), arr1(&[0.0, 1.0, 0.0])];
     let tid = 1;
     let setting = settings::Observation {
@@ -32,15 +30,14 @@ fn io_observation() {
         count: 10,
         r: 1.0,
     };
-    let oid = storage.save_observation(&setting, tid, &obs);
-    let _ = storage.load_observation(oid);
+    let oid = conn.save_observation(&setting, tid, &obs);
+    let _ = conn.load_observation(oid);
 }
 
 #[test]
 fn io_ensemble() {
-    let conn = rusqlite::Connection::open("test.db").unwrap();
-    let storage = sqlite::SqliteStorage::new(&conn);
+    let conn = sqlite::open_with_init("test_io_ensemble.db");
     let ens = vec![arr1(&[1.0, 0.0, 0.0]), arr1(&[0.0, 1.0, 0.0])];
-    let tbname = storage.save_ensemble(&ens);
-    let _ = storage.load_ensemble(tbname);
+    let tbname = conn.save_ensemble(&ens);
+    let _ = conn.load_ensemble(tbname);
 }
