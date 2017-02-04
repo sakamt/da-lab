020f6d767afa0a0b53f9c63089574cc6fbfde418
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-02-04 18:21:07 +0900

    Add open_with_init

--- rust/src/sqlite/util.rs
+++ rust/src/sqlite/util.rs
@@ -1,6 +1,38 @@
 
+use std::path::Path;
 use time;
 use uuid::Uuid;
+use rusqlite::Connection;
+
+const INIT_SQL: &'static str = r#"
+CREATE TABLE IF NOT EXISTS enkf (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  K INTEGER NOT NULL,
+  tau INTEGER NOT NULL,
+  count INTEGER NOT NULL,
+  r REAL NOT NULL,
+  dt REAL NOT NULL,
+  truth_id INTEGER NOT NULL,
+  observation_id INTEGER NOT NULL,
+  ensemble_table TEXT NOT NULL,
+  stat_table TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS truth (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  table_name TEXT NOT NULL,
+  dt REAL NOT NULL,
+  length INTEGER NOT NULL
+);
+CREATE TABLE IF NOT EXISTS observation (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  table_name TEXT NOT NULL,
+  dt REAL NOT NULL,
+  tau INTEGER NOT NULL,
+  count INTEGER NOT NULL,
+  r REAL NOT NULL,
+  truth_id INTEGER NOT NULL
+);
+"#;
 
 pub fn now_str() -> String {
     let tm = time::now();
@@ -9,4 +41,14 @@ pub fn now_str() -> String {
 
 pub fn generate_table_name(prefix: &str) -> String {
     format!("{}_{}", prefix, Uuid::new_v4())
+}
+
+pub fn open_with_init(dbpath: &Path) -> Connection {
+    if dbpath.exists() {
+        Connection::open(dbpath).expect("Failed to open DB")
+    } else {
+        let db = Connection::open(dbpath).expect("Failed to open DB");
+        db.execute(&INIT_SQL, &[]).expect("Failed to init");
+        db
+    }
 }
