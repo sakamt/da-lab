bb244dd00ab23da7ba59815c22141bd5af640018
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-03-14 18:04:07 +0900

    Fix bug (seems to be working...)

--- rust/src/etkf.rs
+++ rust/src/etkf.rs
@@ -30,14 +30,15 @@ impl ETKF {
 impl EnsembleAnalyzer for ETKF {
     fn analysis(&self, xs: Ensemble, y: &V) -> Ensemble {
         let m = xs.len();
+        let mm1 = m as f64 - 1.0;
         let xm = mean(&xs);
         let dxs = hstack(&xs.into_iter().map(|x| x - &xm).collect::<Vec<_>>()).unwrap();
         let dys = hstack(&dxs.axis_iter(Axis(1)).map(|x| self.obs.eval(&x.to_owned())).collect::<Vec<_>>()).unwrap();
         let dy = y - &self.obs.eval(&xm);
-        let p_inv = (m as f64 - 1.0) * self.rho_inv * Array::eye(m) + dys.t().dot(&self.r_inv).dot(&dys);
+        let p_inv = mm1 * self.rho_inv * Array::eye(m) + dys.t().dot(&self.r_inv).dot(&dys);
         let p = p_inv.inv().unwrap();
         let w = p.dot(&dys.t().dot(&self.r_inv.dot(&dy)));
-        let t = p.ssqrt().unwrap();
+        let t = (mm1 * p).ssqrt().unwrap();
         let xm_a = xm + dxs.dot(&w);
         let dxs_a = dxs.dot(&t);
         dxs_a.axis_iter(Axis(1)).map(|dx| &dx + &xm_a).collect()
