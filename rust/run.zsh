45b8f2e62481560ed393590e826452e9ba05a10e
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-03-16 17:21:27 +0900

    Fix run script

--- rust/run.zsh
+++ rust/run.zsh
@@ -5,7 +5,8 @@ setting=$1
 da=$2
 
 time cargo run --release --bin=l63_init $setting
-time cargo run --release --bin=l63_genobs $setting init.msg
+time cargo run --release --bin=l63_truth $setting init.msg
+time cargo run --release --bin=l63_obs $setting truth.msg
 output="${da}_$(date +%Y%m%d-%H%M%S)"
 time cargo run --release --bin=l63_$da $setting obs.msg init.msg $output
 time cargo run --release --bin=l63_rmse truth.msg $output
