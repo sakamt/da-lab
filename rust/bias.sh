c73b29d8e7a7f8d9c7db22b490a6a938b2cc5fca
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-03-23 15:49:12 +0900

    use GNU parallel

--- rust/bias.sh
+++ rust/bias.sh
@@ -9,9 +9,7 @@ cargo run --release --bin=l63_truth $setting init.msg
 output="bias_${da}_$(date +%Y%m%d-%H%M%S)"
 mkdir -p $output
 cp $setting init.msg truth.msg $output
-for i in $(seq 1 $count); do
-  cargo run --release --bin=l63_obs $setting truth.msg > /dev/null 2>&1
-  cargo run --release --bin=l63_${da}_bias $setting truth.msg obs.msg > $output/$i.csv 2>/dev/null
-  echo "$i/$count"
-done
-
+parallel -j $(nproc --all) "echo {}/${count};\
+  cargo run --release --bin=l63_obs $setting truth.msg > /dev/null 2>&1 && \
+  cargo run --release --bin=l63_${da}_bias $setting truth.msg obs.msg > $output/{}.csv 2>/dev/null \
+  " ::: $(seq 1 $count)
