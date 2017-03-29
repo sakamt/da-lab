25aa614d3ad1c5527f38c99d47d8cc4c94994e60
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-03-29 16:18:59 +0900

    Rename start scripts

--- rust/bias.sh
+++ rust/bias.sh
@@ -2,15 +2,11 @@
 
 setting=$1
 da=$2
-count=$3
 
 cargo run --release --bin=l63_init $setting
 cargo run --release --bin=l63_truth $setting init.msg
+cargo run --release --bin=l63_obs $setting truth.msg
 output="bias_${da}_$(date +%Y%m%d-%H%M%S)"
 mkdir -p $output
-cp $setting init.msg truth.msg $output
-parallel -j $(nproc --all) "echo {}/${count};\
-  cargo run --release --bin=l63_obs -- $setting truth.msg --output=obs{}.msg > /dev/null && \
-  cargo run --release --bin=l63_${da}_bias -- $setting truth.msg obs{}.msg > $output/{}.csv 2>/dev/null \
-  " ::: $(seq 1 $count)
-mv obs*.msg $output/
+cp $setting init.msg obs.msg truth.msg $output
+cargo run --release --bin=l63_bias -- $da $setting truth.msg obs.msg > $output/bias.csv
--- rust/bias_en_mean.sh
+++ rust/bias_en_mean.sh
@@ -7,10 +7,10 @@ count=$3
 cargo run --release --bin=l63_init $setting
 cargo run --release --bin=l63_truth $setting init.msg
 cargo run --release --bin=l63_obs $setting truth.msg
-output="bias_fix_obs_${da}_$(date +%Y%m%d-%H%M%S)"
+output="bias_en_mean_${da}_$(date +%Y%m%d-%H%M%S)"
 mkdir -p $output
 cp $setting init.msg truth.msg obs.msg $output
 for i in $(seq 1 $count); do
-  cargo run --release --bin=l63_${da}_bias $setting truth.msg obs.msg > $output/$i.csv 2>/dev/null
+  cargo run --release --bin=l63_bias -- $da $setting truth.msg obs.msg > $output/$i.csv 2>/dev/null
   echo "$i/$count"
 done
