fc3805616ac3abf04dafb6180ccb5cab63cff186
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-10 16:25:51 +0900

    Add max nproc

--- rust/bias_obs_mean.sh
+++ rust/bias_obs_mean.sh
@@ -3,13 +3,15 @@
 setting=$1
 da=$2
 count=$3
+nproc=${4:-$(nproc --all)}
+echo "nproc=$nproc"
 
 cargo run --release --bin=l63_init $setting
 cargo run --release --bin=l63_truth $setting init.msg
 output="bias_obs_mean_${da}_$(date +%Y%m%d-%H%M%S)"
 mkdir -p $output
 cp $setting init.msg truth.msg $output
-parallel -j $(nproc --all) "echo {}/${count};\
+parallel -j $nproc "echo {}/${count};\
   cargo run --release --bin=l63_obs -- $setting truth.msg --output=obs{}.msg > /dev/null && \
   cargo run --release --bin=l63_bias -- $da $setting truth.msg obs{}.msg > $output/{}.csv 2>/dev/null \
   " ::: $(seq 1 $count)
