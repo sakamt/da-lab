6f2547df18b4bff390e11e5cd9c52df2926cc2b8
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-07-10 19:27:47 +0900

    select_model

--- src/model.rs
+++ src/model.rs
@@ -1,18 +1,33 @@
 use ndarray::*;
-use ndarray_odeint::*;
+use ndarray_odeint::{TimeEvolution, explicit, model};
 
+use super::da;
 use super::types::V;
 
 pub struct L63 {
-    param: explicit::RK4<model::lorenz63::Lorenz63, f64>,
+    teo: explicit::RK4<model::lorenz63::Lorenz63, f64>,
     step: usize,
 }
 
 impl<'a> TimeEvolution<OwnedRepr<f64>, Ix1> for &'a L63 {
     fn iterate(self, mut x: &mut V) -> &mut V {
         for _ in 0..self.step {
-            self.param.iterate(&mut x);
+            self.teo.iterate(&mut x);
         }
         x
+    }
+}
+
+pub fn select_model(setting: &da::Setting) -> Box<da::EnsembleForecaster> {
+    match setting.model.as_str() {
+        "l63" => {
+            let p = model::lorenz63::Lorenz63::default();
+            let rk4 = explicit::rk4(p, setting.dt);
+            Box::new(L63 {
+                teo: rk4,
+                step: setting.tau,
+            })
+        }
+        _ => panic!("unsupported model: {}", setting.model),
     }
 }
