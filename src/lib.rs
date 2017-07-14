a9fdb11209060567e8578f5540fa1c91fa553805
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-07-14 15:42:02 +0900

    Create method/ mod

--- src/lib.rs
+++ src/lib.rs
@@ -22,24 +22,16 @@ pub mod stat;
 pub mod observation;
 pub mod weight;
 pub mod lyapunov;
-
-// data assimilation
+pub mod method;
 pub mod da;
-pub mod mpf;
-pub mod enkf;
-pub mod etkf;
-
-// models
 pub mod l63;
-
-// for study
 pub mod bias_correct;
 
 pub fn select_analyzer(method_name: &str, setting: da::Setting) -> Box<da::EnsembleAnalyzer> {
     match method_name {
-        "etkf" => Box::new(etkf::ETKF::from(setting)),
-        "enkf" => Box::new(enkf::EnKF::from(setting)),
-        "mpf" => Box::new(mpf::MPF::from(setting)),
+        "etkf" => Box::new(method::ETKF::from(setting)),
+        "enkf" => Box::new(method::EnKF::from(setting)),
+        "mpf" => Box::new(method::MPF::from(setting)),
         _ => panic!("unsupported method"),
     }
 }
---
+++ src/method/mod.rs
@@ -0,0 +1,9 @@
+pub mod enkf;
+pub mod etkf;
+pub mod mpf;
+
+use super::*;
+
+pub use self::enkf::*;
+pub use self::etkf::*;
+pub use self::mpf::*;
